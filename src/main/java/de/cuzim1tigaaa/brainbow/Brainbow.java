b621266fcc1a8abf9eed8c07b73759ed26ce4e7a
CuzIm1Tigaaa <cuz.tigaaa@gmail.com>
2024-05-08 00:44:28 +0200

    renamed vars

--- src/main/java/de/cuzim1tigaaa/brainbow/Brainbow.java
+++ src/main/java/de/cuzim1tigaaa/brainbow/Brainbow.java
@@ -8,7 +8,6 @@ import lombok.Getter;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -18,15 +17,15 @@ import java.util.Collections;
 public class Brainbow extends JavaPlugin {
 
 	private boolean isRunning;
-	private TeamFile file;
-	private TeamUtils utils;
+	private TeamFile teamFile;
+	private TeamUtils teamUtils;
 
 	@Override
 	public void onEnable() {
 		this.isRunning = false;
 
-		this.file = new TeamFile(this);
-		this.utils = new TeamUtils(this);
+		this.teamFile = new TeamFile(this);
+		this.teamUtils = new TeamUtils(this);
 
 		new Events(this);
 	}
@@ -34,8 +33,8 @@ public class Brainbow extends JavaPlugin {
 	@Override
 	public void onDisable() {
 		this.isRunning = false;
-		if(this.file != null)
-			this.file.saveJson();
+		if(this.teamFile != null)
+			this.teamFile.saveJson();
 	}
 
 	public void startGame() {
@@ -47,11 +46,10 @@ public class Brainbow extends JavaPlugin {
 			p.setFoodLevel(20);
 
 			Bukkit.getScheduler().runTaskTimer(this, () -> {
-				Team t = this.utils.getPlayersTeam(p);
+				Team t = this.teamUtils.getPlayersTeam(p);
 				p.spigot().sendMessage(ChatMessageType.ACTION_BAR,
 						new TextComponent(t.getColor() + String.join("", Collections.nCopies(t.getHealth(), "❤"))));
 			}, 0L, 20L);
 		}
 	}
-
 }
